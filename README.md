ed68da2574367c9eb89c80a07e1a7ef7fbdc528b
MrPhysix <redronnie777@gmail.com>
2023-02-18 23:27:18 +0300

    readme

--- README.md
+++ README.md
@@ -9,4 +9,5 @@
 
 
 [Mr.Stellar Burgers](https://react-burger-ebon.vercel.app/ "Mr.Stellar Burgers")
+
 [Figma Template](https://www.figma.com/file/BDmV3uFnjuQ3077vp9BRhk/React-_-%D0%9F%D1%80%D0%BE%D0%B5%D0%BA%D1%82%D0%BD%D1%8B%D0%B5-%D0%B7%D0%B0%D0%B4%D0%B0%D1%87%D0%B8_external_link-(Copy)?node-id=0%3A1/ "Figma")
--- cypress.config.ts
+++ cypress.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   projectId: 'kouj5r',
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // implement node event listeners here s
     },
   },
 
   component: {
     devServer: {
-      framework: "create-react-app",
-      bundler: "webpack",
+      framework: 'create-react-app',
+      bundler: 'webpack',
     },
   },
 });
